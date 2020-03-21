0d2bfe0e0fba534dd23c65feb8675dd7beb0c1a7
albotw <yann.trou@gmail.com>
2020-03-21 15:41:44 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/com/generic/coreClasses/MapObject.java
+++ src/com/generic/coreClasses/MapObject.java
@@ -73,4 +73,44 @@ public abstract class MapObject implements Movement{
             }
         }
     }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Sprite getSpr() {
+        return spr;
+    }
+
+    public void setSpr(Sprite spr) {
+        this.spr = spr;
+    }
+
+    public Map getM() {
+        return m;
+    }
+
+    public void setM(Map m) {
+        this.m = m;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
--- src/com/generic/player/PlayerThread.java
+++ src/com/generic/player/PlayerThread.java
@@ -3,6 +3,7 @@ package com.generic.player;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import com.generic.coreClasses.MapObject;
+import com.generic.gameplayClasses.Game;
 
 public class PlayerThread extends Thread implements KeyListener {
     private MapObject controlledObject;
@@ -26,5 +27,25 @@ public class PlayerThread extends Thread implements KeyListener {
     public void keyReleased(KeyEvent key)
     {
 
+    }
+
+    public MapObject getControlledObject()
+    {
+        return this.controlledObject;
+    }
+
+    public void setControlledObject(MapObject mo){this.controlledObject = mo;}
+
+    public void removeLive()
+    {
+        currentLives--;
+        if (currentLives <= 0)
+        {
+            Game.gameOver();
+        }
+        else
+        {
+            Game.respawnAnimal();
+        }
     }
 }
