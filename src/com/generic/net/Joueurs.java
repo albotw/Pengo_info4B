e268277fabab34c4652e2981cb211efea3a295e8
wassim <wassim52@hotmail.fr>
2020-03-21 15:40:56 +0100

    Implémentation de joueurs et serveur.

--- src/com/generic/net/Joueurs.java
+++ src/com/generic/net/Joueurs.java
@@ -1,4 +1,24 @@
 package com.generic.net;
 
-public class Joueurs {
+import java.io.IOException;
+import java.io.BufferedInputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+
+public class Joueurs implements Runnable {
+
+    private Socket sock;
+    private PrintWriter writer = null;
+    private BufferedInputStream reader = null;
+
+    public Joueurs(Socket S){
+        Object socket = s;
+    }
+
+
+    @Override
+    public void run() {
+
+    }
 }
--- src/com/generic/net/Serveur.java
+++ src/com/generic/net/Serveur.java
@@ -10,50 +10,39 @@ import java.util.Scanner;
 
 public class Serveur {
 
-       static final int port = 8080;
-       private boolean isRunning = true;
-       public ServerSocket server = null;
+    static final int port = 8080;
+    private boolean isRunning = true;
+    public static ServerSocket server = null;
 
-       public static void main(String[]args) throws IOException {
-           ServerSocket s = new ServerSocket(port);
-           Socket soc = s.accept();
-           System.out.println("SOCKET "+s);
-           System.out.println("SOCKET "+soc);
-           BufferedReader sisr = new BufferedReader(new InputStreamReader(soc.getInputStream()));
-           PrintWriter sisw = new PrintWriter(());
-       }
+    public static void main(String[] args) throws IOException {
+        ServerSocket s = new ServerSocket(port);
+        Socket soc = s.accept();
+        System.out.println("SOCKET " + s);
+        System.out.println("SOCKET " + soc);
+        BufferedReader sisr = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+        PrintWriter sisw = new PrintWriter(("fefe"));
+    }
 
-       public void launch(){
-           Thread th = new Thread(new Runnable(){
-               public void run(){
-                   while(isRunning == true){
-                       try {
-                           Socket client = server.accept(); // on attend connexion joueur
-                           System.out.println("Connexion etablit"); //Une fois reçue, on travaille dans un autre thread.
-                           Thread th = new Thread(new JoueurThread(client));
-                           th.start();
+    public void launch() {
+        Thread th = new Thread(new Runnable() {
+            public void run() {
+                while (isRunning) {
+                    try {
+                        Socket client = server.accept();                    // on attend connexion joueur
+                        System.out.println("Connexion etablit");            //Une fois reçue, on travaille dans un autre thread.
+                        Thread th = new Thread(new Joueurs(Joueur));
+                        th.start();
 
-                               } catch (IOException e) {
-                                   e.printStackTrace();
-                               }
-                           }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
 
-                           try {
-                               server.ARRET();
-                           } catch (IOException e) {
-                               e.printStackTrace();
-                               server = null;
-                           }
-                       }
-                   });
-
-                   th.start();
-               }
-
-               public void ARRET(){
-                   isRunning = false;
-               }
-           }
+        th.start();
+    }
+}
 
 
 
