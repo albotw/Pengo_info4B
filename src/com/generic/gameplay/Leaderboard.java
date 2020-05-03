44e6768b5521c186d364c4d4e5d0178a99cb03c0
wassim <wassim52@hotmail.fr>
2020-05-03 10:36:58 +0200

    ScorePair

--- src/com/generic/gameplay/Leaderboard.java
+++ src/com/generic/gameplay/Leaderboard.java
@@ -50,36 +50,9 @@ public class Leaderboard {
                 tab[i+1]=p.getScore().getPlayer[i];
             }
 
-        }
+        }*/
 
     }
-     */
-}
 
-class ScorePair
-{
-    private String pseudo;
-    private int score;
 
-    public ScorePair(String pseudo, int score)
-    {
-        this.pseudo = pseudo;
-        this.score = score;
-    }
 
-    public String getPseudo() {
-        return pseudo;
-    }
-
-    public void setPseudo(String pseudo) {
-        this.pseudo = pseudo;
-    }
-
-    public int getScore() {
-        return score;
-    }
-
-    public void setScore(int score) {
-        this.score = score;
-    }
-}
